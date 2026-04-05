82e8bebc8f0ad3a65790825cddfeb4f1872323d9
sabeya143111-arch <sabeya143111@gmail.com>
2026-04-05 10:25:37 +0300

    Update app.py

--- SUPPLY CHAIN MANGEMENT/app.py
+++ SUPPLY CHAIN MANGEMENT/app.py
@@ -16,7 +16,7 @@ import plotly.graph_objects as go
 import streamlit as st
 
 st.set_page_config(
-    page_title="Multi‑Company Ops Dashboard",
+    page_title=" Swag Multi‑Company Ops Dashboard",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
