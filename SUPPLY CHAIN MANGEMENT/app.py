00d41fef52c3deba440e496c620fad493b0e21b8
sabeya143111-arch <sabeya143111@gmail.com>
2026-04-08 09:47:46 +0300

    Update app.py

--- SUPPLY CHAIN MANGEMENT/app.py
+++ SUPPLY CHAIN MANGEMENT/app.py
@@ -25,8 +25,8 @@ import plotly.graph_objects as go
 import streamlit as st
 
 st.set_page_config(
-    page_title="Swag Executive",
-    page_icon="💎",
+    page_title="Swag Side bar",
+    page_icon="@",
     layout="wide",
     initial_sidebar_state="expanded",
 )
