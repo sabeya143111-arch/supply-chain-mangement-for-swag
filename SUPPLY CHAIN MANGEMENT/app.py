d7a1b87c750a0fc7bdb623acb33045a5fd75400a
sabeya143111-arch <sabeya143111@gmail.com>
2026-04-11 15:44:32 +0300

    Update app.py

--- SUPPLY CHAIN MANGEMENT/app.py
+++ SUPPLY CHAIN MANGEMENT/app.py
@@ -15,7 +15,7 @@ import streamlit as st
 st.set_page_config(
     page_title="SWAG Executive Dashboard",
     page_icon="",
-    layout="Widely",
+    layout="wide",
     initial_sidebar_state="expanded",
 )
 
